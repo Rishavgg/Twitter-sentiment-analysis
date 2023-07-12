9e0c29af9d5790f1fee50e2b869817aa71d66c48
Rishavgg <rishavdadwal1@gmail.com>
2023-07-12 15:08:07 +0530

    updated

--- app.py
+++ app.py
@@ -8,6 +8,7 @@ nltk.download('stopwords')
 ps=PorterStemmer()
 #background image
 import base64
+nltk.download('punkt')
 def add_bg_from_local(image_file):
     with open(image_file, "rb") as image_file:
         encoded_string = base64.b64encode(image_file.read())
